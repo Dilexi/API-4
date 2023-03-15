efe23661066ab7cc9f6b95ae4f0ccaf891f3e623
Dilexi <104418830+Dilexi@users.noreply.github.com>
2023-03-15 11:37:38 +0300

    Исправил работу функции добавив argparse

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from pathlib import Path
 
 import requests
@@ -7,14 +8,20 @@ from dotenv import load_dotenv
 from download_image import download_image
 
 
-def fetch_spacex_last_launch(folder_name):
-    image_link = "https://api.spacexdata.com/v5/launches/"
+def fetch_spacex_last_launch(folder_name, launch_id=None):
+    if launch_id:
+        image_link = f"https://api.spacexdata.com/v5/launches/{launch_id}"
+    else:
+        image_link = "https://api.spacexdata.com/v5/launches/"
     response = requests.get(image_link)
     response.raise_for_status()
     
-    for link_photo_spasex in response.json():
-        if link_photo_spasex["links"]['flickr']["original"]:
-            urls_photo=link_photo_spasex["links"]['flickr']["original"]
+    if launch_id:
+        urls_photo=response.json()['links']['flickr']['original']
+    else:
+        for link_photo_spasex in response.json():
+            if link_photo_spasex["links"]['flickr'] and link_photo_spasex["links"]['flickr']["original"]:
+                urls_photo=link_photo_spasex["links"]['flickr']["original"]
 
     for number, link in enumerate(urls_photo):
         file_name = f"spacex_{number}.jpg"
@@ -24,9 +31,19 @@ def fetch_spacex_last_launch(folder_name):
 
 def main():
     load_dotenv()
+    parser = argparse.ArgumentParser(
+        description='Эта программа позволит вам загрузитть фотографии с запуска SpaceX.'
+    )
+    parser.add_argument(
+        '--id',
+        dest='launch_id',
+        default=None,
+        help='Укажите ID запуска SpaceX, с которого можно загрузить фотографии.'
+    )
+    args = parser.parse_args()
     folder_name = os.environ['FOLDER_SPACEX']
     Path(folder_name).mkdir(parents=True, exist_ok=True)
-    fetch_spacex_last_launch(folder_name)
+    fetch_spacex_last_launch(folder_name, args.launch_id)
 
 
 if __name__=="__main__":
