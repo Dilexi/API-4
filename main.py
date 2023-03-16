ea84e0befa637903af09e543739f44e70cf8e659
Dilexi <104418830+Dilexi@users.noreply.github.com>
2023-03-16 15:15:37 +0300

    перенёс переменные в def

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import random
 from os import listdir
 from time import sleep
 
-TG_TOKEN = os.environ['TG_TOKEN']
-TG_CHAT_ID = os.environ['TG_CHAT_ID']
-FOLDER_SPACEX = os.environ["FOLDER_SPACEX"]
-FOLDER_NASA = os.environ["FOLDER_NASA_APOD"]
-FOLDER_EPIC = os.environ["FOLDER_EPIC"]
-PUBLISH_DELAY = int(os.environ.get('PUBLISH_DELAY', 14400))
 
 def main():
+    TG_TOKEN = os.environ['TG_TOKEN']
+    TG_CHAT_ID = os.environ['TG_CHAT_ID']
+    FOLDER_SPACEX = os.environ["FOLDER_SPACEX"]
+    FOLDER_NASA = os.environ["FOLDER_NASA_APOD"]
+    FOLDER_EPIC = os.environ["FOLDER_EPIC"]
+    PUBLISH_DELAY = int(os.environ.get('PUBLISH_DELAY', 14400))
     bot = telegram.Bot(token=TG_TOKEN)
     while True:
         folders = [
