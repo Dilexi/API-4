f27162dfc82c1bbbf16a7626c909184639670649
Dilexi <104418830+Dilexi@users.noreply.github.com>
2023-03-16 16:13:43 +0300

    добавил значение по умолчанию

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -36,7 +36,7 @@ def get_epic_nasa_images(folder_name, nasa_api_key):
 def main():
     load_dotenv()
     nasa_api_key = os.environ['NASA_API']
-    folder_name = os.environ['FOLDER_EPIC']
+    folder_name = os.environ.get("FOLDER_EPIC", "epic_photos")
     Path(folder_name).mkdir(parents=True, exist_ok=True)
     get_epic_nasa_images(folder_name, nasa_api_key)
 
