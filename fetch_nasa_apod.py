da4b1653f5fff4d3dfbe2e81f7dfb3c1371fa226
Dilexi <104418830+Dilexi@users.noreply.github.com>
2023-03-19 14:18:05 +0300

    изменил переменную окружения

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -43,7 +43,7 @@ def get_nasa_images(folder_nasa, nasa_api_key, folder_name):
 
 def main():
     load_dotenv()
-    nasa_api_key = os.environ['NASA_API']
+    nasa_api_key = os.environ['NASA_TOKEN']
     folder_name = os.environ.get("FOLDER_NASA_APOD", "nasa_apod_photos")
     Path(folder_name).mkdir(parents=True, exist_ok=True)
     get_nasa_images(folder_name, nasa_api_key,folder_name)
